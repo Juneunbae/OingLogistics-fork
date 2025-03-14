3048807bc6eece6873deb3826f1c68d73dc429dd
LeeJieon <dlwldjs7544@naver.com>
2025-03-14 22:50:48 +0900

    Initial commit #45

---
+++ hub-service/src/test/java/com/oingmaryho/business/hubservice/HubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oingmaryho.business.hubservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
