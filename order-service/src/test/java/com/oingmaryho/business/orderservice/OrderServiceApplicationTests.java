eeaa8d81af54ddd8a3cf3499db9c73a823f53d73
juneunbae <dmsqo1403@naver.com>
2025-03-14 17:11:08 +0900

    Initial commit #6

---
+++ order-service/src/test/java/com/oingmaryho/business/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oingmaryho.business.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
