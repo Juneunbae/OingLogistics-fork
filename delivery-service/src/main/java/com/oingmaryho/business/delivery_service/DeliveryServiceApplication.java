05d887c834c6bc269f6b50fcfffa767cddc4d309
haisley77 <hahi1108@gmail.com>
2025-03-15 22:13:53 +0900

    mod: delivery - @EnableJpaAuditing 어노테이션 제거#15
    - MockMVC 테스트 시 Jpa 관련 빈 등록 에러 문제 방지

--- delivery-service/src/main/java/com/oingmaryho/business/delivery_service/DeliveryServiceApplication.java
+++ delivery-service/src/main/java/com/oingmaryho/business/delivery_service/DeliveryServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableJpaAuditing
 public class DeliveryServiceApplication {
 
 	public static void main(String[] args) {
