38555d15dd17e2489a191304a270ab8257c87aed
haisley77 <hahi1108@gmail.com>
2025-03-15 17:05:16 +0900

    feat: delivery - controller, service, repository 껍데기 api 연결#15
    - 비즈니스 로직 미포함
    - api requestDto, serviceDto mapper 활용해 변환 및 service 계층과 연결
    - DeliveryRepository service 계층에 주입

---
+++ delivery-service/src/main/java/com/oingmaryho/business/delivery_service/infrastructure/DeliveryRepository.java
@@ -0,0 +1,9 @@
+package com.oingmaryho.business.delivery_service.infrastructure;
+
+import com.oingmaryho.business.delivery_service.domain.Delivery;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface DeliveryRepository extends JpaRepository<Delivery, UUID> {
+}
