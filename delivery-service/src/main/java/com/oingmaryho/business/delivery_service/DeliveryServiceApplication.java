8caeb23a03ad4fdaf8f35dda5668b8a1e346c631
haisley77 <hahi1108@gmail.com>
2025-03-15 16:56:57 +0900

    feat: delivery - eureka client, jpa auditing 설정#15
    - 어노테이션 추가

--- delivery-service/src/main/java/com/oingmaryho/business/delivery_service/DeliveryServiceApplication.java
+++ delivery-service/src/main/java/com/oingmaryho/business/delivery_service/DeliveryServiceApplication.java
@@ -2,8 +2,12 @@ package com.oingmaryho.business.delivery_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableJpaAuditing
 public class DeliveryServiceApplication {
 
 	public static void main(String[] args) {
