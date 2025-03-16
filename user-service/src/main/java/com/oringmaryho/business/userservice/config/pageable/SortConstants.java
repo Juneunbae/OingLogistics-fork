17b08bc2a1706d1cb94e9e075d90ea55ec1b357e
oneul0 <neul.dev@gmail.com>
2025-03-16 21:37:31 +0900

    feat: pageable config 추가

---
+++ user-service/src/main/java/com/oringmaryho/business/userservice/config/pageable/SortConstants.java
@@ -0,0 +1,6 @@
+package com.oringmaryho.business.userservice.config.pageable;
+
+public class SortConstants {
+  public static final String CREATED_AT = "createdAt";
+  public static final String UPDATED_AT = "updatedAt";
+}
