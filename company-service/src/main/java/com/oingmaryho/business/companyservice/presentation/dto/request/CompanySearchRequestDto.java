799929774d4d5eabbdd0c93009f535ee7216473e
ybon1107 <leeyebon7929@gmail.com>
2025-03-16 17:49:38 +0900

    fix: company - CompanySearchRequestDto 수정#74
    - id 누락 -> id 추가

--- company-service/src/main/java/com/oingmaryho/business/companyservice/presentation/dto/request/CompanySearchRequestDto.java
+++ company-service/src/main/java/com/oingmaryho/business/companyservice/presentation/dto/request/CompanySearchRequestDto.java
@@ -1,6 +1,9 @@
 package com.oingmaryho.business.companyservice.presentation.dto.request;
 
+import java.util.UUID;
+
 public record CompanySearchRequestDto(
+	UUID id,
 	String type,
 	String name
 ) {
