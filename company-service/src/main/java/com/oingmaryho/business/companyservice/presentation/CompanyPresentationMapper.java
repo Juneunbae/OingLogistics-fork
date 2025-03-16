9612ca5d2a132f8dfbeb818f48677273ce59bebb
ybon1107 <leeyebon7929@gmail.com>
2025-03-16 19:38:14 +0900

    feat: company - mapper 설정 추가#74
    - to Application request dto 추가
    - to Presentation response dto 추가
    - build.gradle mapper 의존성 추가

---
+++ company-service/src/main/java/com/oingmaryho/business/companyservice/presentation/CompanyPresentationMapper.java
@@ -0,0 +1,32 @@
+package com.oingmaryho.business.companyservice.presentation;
+import java.awt.print.Pageable;
+import java.util.UUID;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import com.oingmaryho.business.companyservice.application.dto.request.CompanyCreateRequestServiceDto;
+import com.oingmaryho.business.companyservice.application.dto.request.CompanyDeleteRequestServiceDto;
+import com.oingmaryho.business.companyservice.application.dto.request.CompanyDetailsSearchRequestServiceDto;
+import com.oingmaryho.business.companyservice.application.dto.request.CompanySearchRequestServiceDto;
+import com.oingmaryho.business.companyservice.application.dto.request.CompanyUpdateRequestServiceDto;
+import com.oingmaryho.business.companyservice.presentation.dto.request.CompanyCreateRequestDto;
+import com.oingmaryho.business.companyservice.presentation.dto.request.CompanySearchRequestDto;
+import com.oingmaryho.business.companyservice.presentation.dto.request.CompanyUpdateRequestDto;
+
+@Mapper(componentModel = "spring")
+public interface CompanyPresentationMapper {
+	CompanyPresentationMapper INSTANCE = Mappers.getMapper(CompanyPresentationMapper.class);
+
+	CompanyCreateRequestServiceDto toCreateServiceDto(CompanyCreateRequestDto companyCreateRequestDto);
+
+	CompanyDetailsSearchRequestServiceDto toDetailsSearchServiceDto(UUID id);
+
+	CompanySearchRequestServiceDto toSearchServiceDto(CompanySearchRequestDto companySearchRequestDto, Pageable pageable);
+
+	@Mapping(target="id",source="id")
+	CompanyUpdateRequestServiceDto toUpdateServiceDto(UUID id, CompanyUpdateRequestDto companyUpdateRequestDto);
+
+	CompanyDeleteRequestServiceDto toDeleteServiceDto(UUID id);
+}
