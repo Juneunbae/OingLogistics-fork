e32bb157a9460eedb038539d0442ece3b2b8eefb
ybon1107 <leeyebon7929@gmail.com>
2025-03-16 19:59:17 +0900

    feat: company - pageable 설정 추가#74
    - pageable config 추가

---
+++ company-service/src/main/java/com/oingmaryho/business/companyservice/config/pageable/SortConstants.java
@@ -0,0 +1,6 @@
+package com.oingmaryho.business.companyservice.config.pageable;
+
+public class SortConstants {
+	public static final String CREATED_AT = "createdAt";
+	public static final String UPDATED_AT = "updatedAt";
+}
