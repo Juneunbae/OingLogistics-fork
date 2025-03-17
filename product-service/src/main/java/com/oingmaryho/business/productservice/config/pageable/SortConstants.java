3a7ef288b92a4ca38fe0ae818ddf290c97b02eb0
ybon1107 <leeyebon7929@gmail.com>
2025-03-17 15:33:04 +0900

    feat: product - 초기 설정#73
    - feignClient 설정
    - pageable config 설정
    - Jpa Auditing config 설정
    - BaseEntity 추가
    - build.gradle mapstruct 추가

---
+++ product-service/src/main/java/com/oingmaryho/business/productservice/config/pageable/SortConstants.java
@@ -0,0 +1,6 @@
+package com.oingmaryho.business.productservice.config.pageable;
+
+public class SortConstants {
+	public static final String CREATED_AT = "createdAt";
+	public static final String UPDATED_AT = "updatedAt";
+}
---
+++ product-service/src/main/java/com/oingmaryho/business/productservice/domain/BaseEntity.java
@@ -0,0 +1,44 @@
+package com.oingmaryho.business.productservice.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@SuperBuilder
+@MappedSuperclass
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+	@Column(updatable = false)
+	private Long deletedBy;
+
+	@Column(updatable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private LocalDateTime deletedAt;
+
+	@CreatedDate
+	@Column(updatable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private LocalDateTime createdAt;
+
+	@CreatedBy
+	@Column(updatable = false)
+	private Long createdBy;
+
+	@LastModifiedDate
+	@Column
+	@Temporal(TemporalType.TIMESTAMP)
+	private LocalDateTime updatedAt;
+
+	@LastModifiedBy
+	private Long updatedBy;
+}
