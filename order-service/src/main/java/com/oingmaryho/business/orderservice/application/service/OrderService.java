f8a59b13e7ed93b40dfb8826adbf4f06952f04bd
juneunbae <dmsqo1403@naver.com>
2025-03-18 16:02:17 +0900

    feat: 주문 전체 조회 기능 구현
    1. build.gradle
    - apache commons 추가
    - QueryDSL 설정 추가
    
    2. PageableUtils
    - page 설정 추가
    
    3. QueryDslConfig
    - 초기 설정 추가
    
    4. QueryDslUtils
    - 초기 설정 추가
    
    5. OrderController
    - getOrders 추가
    
    6. OrderService
    - getOrders 추가
    
    7. OrderApplicationMapper
    - toOrderDto -> toOrderResponseServiceDto 수정

--- order-service/src/main/java/com/oingmaryho/business/orderservice/application/service/OrderService.java
+++ order-service/src/main/java/com/oingmaryho/business/orderservice/application/service/OrderService.java
@@ -1,5 +1,7 @@
 package com.oingmaryho.business.orderservice.application.service;
 
+import com.oingmaryho.business.orderservice.application.dto.mapper.OrderApplicationMapper;
+import com.oingmaryho.business.orderservice.application.dto.request.OrdersRequestServiceDto;
 import com.oingmaryho.business.orderservice.domain.Order;
 import com.oingmaryho.business.orderservice.domain.OrderDetail;
 import com.oingmaryho.business.orderservice.exception.ErrorCode;
@@ -11,9 +13,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
@@ -22,7 +29,28 @@ import java.util.UUID;
 public class OrderService {
     private final CacheManager cacheManager;
     private final OrderRepository orderRepository;
-    private final OrderPresentationMapper orderPresentationMapper;
+    private final OrderApplicationMapper orderApplicationMapper;
+
+    @Transactional(readOnly = true)
+    @Cacheable(cacheNames = "orders")
+    public Page<OrderResponseServiceDto> getOrders(OrdersRequestServiceDto ordersRequestServiceDto) {
+        // TODO: Role - HubManager, HubDeliveryManager, CompanyDeliveryManager, CompanyManager 설정하기
+
+        Pageable customPageable = ordersRequestServiceDto.customPageable();
+        Page<Order> orders = orderRepository.findAll(customPageable);
+        // TODO: QueryDSL 반영하여 LIKE 문 수정하기
+
+        List<OrderResponseServiceDto> ordersDto = orders.stream().map(
+            order -> orderApplicationMapper.toOrderResponseServiceDto(
+                order,
+                order.getOrderDetails().stream().map(
+                    orderDetail -> orderApplicationMapper.toOrderDetailDto(order.getId(), orderDetail)
+                ).toList()
+            )
+        ).toList();
+
+        return new PageImpl<>(ordersDto, customPageable, orders.getTotalElements());
+    }
 
     public Order getByOrderId(UUID orderId) {
         return orderRepository.findById(orderId)
---
+++ order-service/src/main/java/com/oingmaryho/business/orderservice/utils/QueryDslUtils.java
@@ -0,0 +1,20 @@
+package com.oingmaryho.business.orderservice.utils;
+
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
+import org.springframework.data.domain.Sort;
+
+public class QueryDslUtils {
+    public static OrderSpecifier<?>[] getOrderSpecifiers(Sort sort, Class<?> entityClass) {
+        PathBuilder<?> entityPath = new PathBuilder<>(entityClass, entityClass.getSimpleName().toLowerCase());
+        return sort.stream()
+            .map(order -> new OrderSpecifier<>(
+                    order.isAscending() ? Order.ASC : Order.DESC,
+                    entityPath.getString(order.getProperty())
+                )
+            )
+            .toList()
+            .toArray(new OrderSpecifier[0]);
+    }
+}
