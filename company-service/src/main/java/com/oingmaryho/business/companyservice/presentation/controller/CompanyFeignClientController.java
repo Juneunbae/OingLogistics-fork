5806641d6f0d2f494333ae375174c7a9120453ff
ybon1107 <leeyebon7929@gmail.com>
2025-03-19 02:40:59 +0900

    fix : feignClient path variable 수정#103

--- company-service/src/main/java/com/oingmaryho/business/companyservice/presentation/controller/CompanyFeignClientController.java
+++ company-service/src/main/java/com/oingmaryho/business/companyservice/presentation/controller/CompanyFeignClientController.java
@@ -26,7 +26,7 @@ public class CompanyFeignClientController {
 	private final CompanyPresentationMapper companyPresentationMapper;
 
 	@Description("FeignClient - 업체 상세 조회")
-	@GetMapping("{/id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<CompanyDetailsSearchResponseDto> getCompanyById(@PathVariable UUID id) {
 		CompanyDetailsSearchRequestServiceDto requestServiceDto = companyPresentationMapper.toDetailsSearchServiceDto(id);
 		CompanyDetailsSearchResponseServiceDto responseServiceDto = companyFeignService.getCompanyById(requestServiceDto);
