93f63d11bc807faabfe8386b153af87eb9a06361
oneul0 <neul.dev@gmail.com>
2025-03-22 21:23:53 +0900

    feat: slack project init#169

---
+++ slack-service/src/main/java/com/oringmaryho/business/slackservice/SlackServiceApplication.java
@@ -0,0 +1,13 @@
+package com.oringmaryho.business.slackservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SlackServiceApplication.class, args);
+  }
+
+}
---
+++ slack-service/src/test/java/com/oringmaryho/business/slackservice/SlackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oringmaryho.business.slackservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
