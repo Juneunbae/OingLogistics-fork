71f22601347b6057d9e09ba0ce5033a753b0848e
ybon1107 <leeyebon7929@gmail.com>
2025-03-22 18:23:19 +0900

    feat: product message queue 설정#163
    - String event 에 대한 value 가져와 처리

--- product-service/src/main/java/com/oingmaryho/business/productservice/config/rabbitMQ/RabbitMQConfig.java
+++ product-service/src/main/java/com/oingmaryho/business/productservice/config/rabbitMQ/RabbitMQConfig.java
@@ -2,11 +2,20 @@ package com.oingmaryho.business.productservice.config.rabbitMQ;
 
 import org.springframework.amqp.core.Queue;
 
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+	@Bean
+	public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+		return rabbitTemplate;
+	}
 
 	@Bean
 	public Queue queueErrProduct() {
