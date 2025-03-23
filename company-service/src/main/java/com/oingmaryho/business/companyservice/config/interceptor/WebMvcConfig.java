2b9be34ec6fd0768ac4a0eec0928d454e4139d98
ybon1107 <leeyebon7929@gmail.com>
2025-03-23 15:42:41 +0900

    feat: company-service WebMvcConfig 추가#134
    - WebMvcConfig 추가

---
+++ company-service/src/main/java/com/oingmaryho/business/companyservice/config/interceptor/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.oingmaryho.business.companyservice.config.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.oingmaryho.business.common.presentation.interceptor.AdminCheckInterceptor;
+import com.oingmaryho.business.common.presentation.interceptor.UserCheckInterceptor;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	private final RedisTemplate<String, Object> redisTemplate;
+
+	public WebMvcConfig(RedisTemplate<String, Object> redisTemplate) {
+		this.redisTemplate = redisTemplate;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new UserCheckInterceptor(redisTemplate));
+		registry.addInterceptor(new AdminCheckInterceptor(redisTemplate))
+			.excludePathPatterns("/api/**");
+	}
+
+}
