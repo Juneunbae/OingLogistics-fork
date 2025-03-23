75ef6b2954bdc308af3a8f23b7749370c6912160
ybon1107 <leeyebon7929@gmail.com>
2025-03-23 21:39:36 +0900

    fix: interceptor exception 수정#183

--- common/src/main/java/com/oingmaryho/business/common/exception/CommonErrorCode.java
+++ common/src/main/java/com/oingmaryho/business/common/exception/CommonErrorCode.java
@@ -8,7 +8,13 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum CommonErrorCode implements BaseErrorCode {
 
-	FORBIDDEN("B-001", "권한이 없습니다.", HttpStatus.FORBIDDEN);
+	FORBIDDEN("B-001", "권한이 없습니다.", HttpStatus.FORBIDDEN),
+	UNAUTHORIZED("B-002", "인증되지 않은 사용자입니다.", HttpStatus.UNAUTHORIZED),
+	USER_NOT_FOUND("B-003", "사용자 정보를 찾을 수 없습니다.", HttpStatus.UNAUTHORIZED),
+	INVALID_USER_STATUS("B-004", "확인되지 않은 사용자입니다.", HttpStatus.UNAUTHORIZED),
+	INVALID_ROLE("B-005", "해당 작업을 수행할 수 없는 역할입니다.", HttpStatus.FORBIDDEN),
+	MISSING_USER_INFO("B-006", "사용자 정보가 유실되었습니다.", HttpStatus.UNAUTHORIZED),
+	;
 
 	private final String errorCode;
 	private final String message;
