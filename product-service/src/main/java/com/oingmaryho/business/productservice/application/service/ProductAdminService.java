080a1dc1cc3d5c12bbf15f5c6ae2fe3946aa3844
ybon1107 <leeyebon7929@gmail.com>
2025-03-24 13:42:50 +0900

    feat: product admin api 추가#190
    - admin api product 추가

---
+++ product-service/src/main/java/com/oingmaryho/business/productservice/application/service/ProductAdminService.java
@@ -0,0 +1,170 @@
+package com.oingmaryho.business.productservice.application.service;
+
+import java.util.UUID;
+
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.oingmaryho.business.common.domain.type.UserRoleType;
+import com.oingmaryho.business.productservice.application.dto.request.ProductCreateRequestServiceDto;
+import com.oingmaryho.business.productservice.application.dto.request.ProductDeleteRequestServiceDto;
+import com.oingmaryho.business.productservice.application.dto.request.ProductDetailsSearchRequestServiceDto;
+import com.oingmaryho.business.productservice.application.dto.request.ProductSearchRequestServiceDto;
+import com.oingmaryho.business.productservice.application.dto.request.ProductUpdateRequestServiceDto;
+import com.oingmaryho.business.productservice.application.dto.response.ProductCreateResponseServiceDto;
+import com.oingmaryho.business.productservice.application.dto.response.ProductDetailsSearchResponseServiceDto;
+import com.oingmaryho.business.productservice.application.dto.response.ProductSearchResponseServiceDto;
+import com.oingmaryho.business.productservice.application.dto.response.ProductUpdateResponseServiceDto;
+import com.oingmaryho.business.productservice.application.mapper.ProductApplicationMapper;
+import com.oingmaryho.business.productservice.application.service.feignClient.CompanyClient;
+import com.oingmaryho.business.productservice.application.service.feignClient.HubClient;
+import com.oingmaryho.business.productservice.config.cache.CacheType;
+import com.oingmaryho.business.productservice.domain.Product;
+import com.oingmaryho.business.productservice.domain.ProductSearchCriteria;
+import com.oingmaryho.business.productservice.domain.repository.CustomProductRepository;
+import com.oingmaryho.business.productservice.domain.repository.ProductRepository;
+import com.oingmaryho.business.productservice.exception.ErrorCode;
+import com.oingmaryho.business.productservice.exception.ProductException;
+import com.oingmaryho.business.productservice.presentation.dto.response.CompanyDetailsSearchResponseDto;
+import com.oingmaryho.business.productservice.presentation.dto.response.HubSearchResponseDto;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProductAdminService {
+	private final CompanyClient companyClient;
+	private final HubClient hubClient;
+	private final ProductRepository productRepository;
+	private final CustomProductRepository customProductRepository;
+	private final ProductApplicationMapper productApplicationMapper;
+
+	@Transactional
+	public ProductCreateResponseServiceDto createProduct(
+		ProductCreateRequestServiceDto productCreateRequestServiceDto
+	){
+		// company, hub 존재 확인
+		CompanyDetailsSearchResponseDto company = companyClient(productCreateRequestServiceDto.companyId());
+		// HubSearchResponseDto hub = hubClient(productCreateRequestServiceDto.manageHubId());
+		validateSupplierCompany(company);
+
+		String productCode = productCreateRequestServiceDto.productCode();
+		if (productRepository.existsByProductCodeAndIsDeletedFalse(productCode)) {
+			throw new ProductException(ErrorCode.ALREADY_REGISTERED_PRODUCT);
+		}
+
+		Product product = productApplicationMapper.toCreateEntity(productCreateRequestServiceDto,company.name());
+		Product saveProduct = productRepository.save(product);
+		return new ProductCreateResponseServiceDto(saveProduct.getId());
+	}
+
+	public Page<ProductSearchResponseServiceDto> searchProducts(
+		ProductSearchRequestServiceDto requestDto,
+		Pageable pageable
+	){
+		validateSearchCriteria(createProductSearchCriteria(requestDto));
+		Page<Product> products = customProductRepository.findDynamicQuery(createProductSearchCriteria(requestDto),pageable);
+		return products.map(productApplicationMapper::toProductSearchResponseServiceDto);
+	}
+
+	@Cacheable(
+		cacheNames = CacheType.PRODUCT_CACHE,
+		key = "'productDetails:' + #requestDto.id() + ': admin'"
+	)
+	public ProductDetailsSearchResponseServiceDto getProductDetails(
+		ProductDetailsSearchRequestServiceDto requestDto
+	) {
+		Product product = productRepository.findByIdAndIsDeletedFalse(requestDto.id())
+			.orElseThrow(() -> new ProductException(ErrorCode.NOT_FOUND));
+
+		return productApplicationMapper.toResponseDto(product);
+	}
+
+	@CacheEvict(
+		cacheNames = CacheType.PRODUCT_CACHE,
+		key = "'productDetails:' + #requestServiceDto.id() + ': admin'"
+	)
+	@Transactional
+	public ProductUpdateResponseServiceDto updateProduct(
+		ProductUpdateRequestServiceDto requestServiceDto
+	) {
+		Product product = productRepository.findByIdAndIsDeletedFalse(requestServiceDto.id())
+			.orElseThrow(() -> new ProductException(ErrorCode.NOT_FOUND));
+
+		//허브 존재하는지
+
+		product.update(
+			requestServiceDto.manageHubId(),
+			requestServiceDto.name(),
+			requestServiceDto.price(),
+			requestServiceDto.stock()
+		);
+		return productApplicationMapper.toUpdateResponseDto(product.getId());
+	}
+
+	@CacheEvict(
+		cacheNames = CacheType.PRODUCT_CACHE,
+		key = "'productDetails:' + #requestServiceDto.id() + ': admin'"
+	)
+	@Transactional
+	public void deleteProduct(
+		ProductDeleteRequestServiceDto requestServiceDto,
+		Long userId
+	) {
+		Product product = productRepository.findByIdAndIsDeletedFalse(requestServiceDto.id())
+			.orElseThrow(() -> new ProductException(ErrorCode.NOT_FOUND));
+
+		product.softDelete(userId);
+	}
+
+	private ProductSearchCriteria createProductSearchCriteria(ProductSearchRequestServiceDto requestDto){
+		return ProductSearchCriteria.builder()
+			.id(requestDto.id())
+			.productCode(requestDto.productCode())
+			.name(requestDto.name())
+			.manageHubId(requestDto.manageHubId())
+			.companyID(requestDto.companyId())
+			.companyName(requestDto.companyName())
+			.minPrice(requestDto.minPrice())
+			.maxPrice(requestDto.maxPrice())
+			.minStock(requestDto.minStock())
+			.maxStock(requestDto.maxStock())
+			.isDeleted(requestDto.isDeleted())
+			.build();
+	}
+	private void validateSearchCriteria(ProductSearchCriteria searchCriteria) {
+		if (searchCriteria.getMinPrice() != null && searchCriteria.getMaxPrice() != null) {
+			if (searchCriteria.getMinPrice() > searchCriteria.getMaxPrice()) {
+				throw new ProductException(ErrorCode.INVALID_PRICE_RANGE);
+			}
+		}
+
+		if (searchCriteria.getMinStock() != null && searchCriteria.getMaxStock() != null) {
+			if (searchCriteria.getMinStock() > searchCriteria.getMaxStock()) {
+				throw new ProductException(ErrorCode.INVALID_STOCK_RANGE);
+			}
+		}
+	}
+
+	private void validateSupplierCompany(CompanyDetailsSearchResponseDto company) {
+
+		if (!company.type().equals("SUPPLIER")) {
+			throw new ProductException(ErrorCode.INVALID_COMPANY_TYPE);
+		}
+	}
+
+	private CompanyDetailsSearchResponseDto companyClient(UUID companyId) {
+		return companyClient.isManagerOfCompany(companyId)
+			.orElseThrow(() -> new ProductException(ErrorCode.COMPANY_NOT_FOUND));
+
+	}
+
+	// private HubSearchResponseDto hubClient(UUID manageHubId) {
+	// 	return hubClient.isManagerOfHub(manageHubId)
+	// }
+
+}
---
+++ product-service/src/main/java/com/oingmaryho/business/productservice/presentation/controller/ProductAdminController.java
@@ -0,0 +1,132 @@
+package com.oingmaryho.business.productservice.presentation.controller;
+
+import java.util.UUID;
+
+import org.springframework.context.annotation.Description;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.oingmaryho.business.common.domain.type.UserRoleType;
+import com.oingmaryho.business.common.infrastructure.annotation.RequiredRoles;
+import com.oingmaryho.business.productservice.application.dto.request.ProductCreateRequestServiceDto;
+import com.oingmaryho.business.productservice.application.dto.request.ProductDeleteRequestServiceDto;
+import com.oingmaryho.business.productservice.application.dto.request.ProductDetailsSearchRequestServiceDto;
+import com.oingmaryho.business.productservice.application.dto.request.ProductUpdateRequestServiceDto;
+import com.oingmaryho.business.productservice.application.dto.response.ProductCreateResponseServiceDto;
+import com.oingmaryho.business.productservice.application.dto.response.ProductDetailsSearchResponseServiceDto;
+import com.oingmaryho.business.productservice.application.dto.response.ProductSearchResponseServiceDto;
+import com.oingmaryho.business.productservice.application.dto.response.ProductUpdateResponseServiceDto;
+import com.oingmaryho.business.productservice.application.service.ProductAdminService;
+import com.oingmaryho.business.productservice.presentation.dto.request.ProductCreateRequestDto;
+import com.oingmaryho.business.productservice.presentation.dto.request.ProductSearchRequestDto;
+import com.oingmaryho.business.productservice.presentation.dto.request.ProductUpdateRequestDto;
+import com.oingmaryho.business.productservice.presentation.dto.response.ProductCreateResponseDto;
+import com.oingmaryho.business.productservice.presentation.dto.response.ProductDetailsSearchResponseDto;
+import com.oingmaryho.business.productservice.presentation.dto.response.ProductSearchResponseDto;
+import com.oingmaryho.business.productservice.presentation.dto.response.ProductUpdateResponseDto;
+import com.oingmaryho.business.productservice.presentation.mapper.ProductPresentationMapper;
+import com.oingmaryho.business.productservice.utils.PageableUtils;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin/v1/products")
+public class ProductAdminController {
+
+	private final ProductAdminService productService;
+	private final ProductPresentationMapper productPresentationMapper;
+
+	@Description("마스터 - 상품 등록")
+	@RequiredRoles(UserRoleType.MASTER)
+	@PostMapping
+	public ResponseEntity<ProductCreateResponseDto> createProduct(
+		@RequestBody ProductCreateRequestDto productCreateRequestDto
+	) {
+
+		ProductCreateRequestServiceDto requestServiceDto = productPresentationMapper.toCreateServiceDto(productCreateRequestDto);
+		ProductCreateResponseServiceDto responseServiceDto = productService.createProduct(requestServiceDto);
+		ProductCreateResponseDto responseDto = productPresentationMapper.toCreateDto(responseServiceDto);
+		return ResponseEntity.ok(responseDto);
+	}
+
+	@Description("마스터 - 상품 전체 조회")
+	@RequiredRoles(UserRoleType.MASTER)
+	@GetMapping
+	public ResponseEntity<Page<ProductSearchResponseDto>> getProducts(
+		@RequestParam(name = "page", defaultValue = "0") int page,
+		@RequestParam(name = "size", defaultValue = "10") int size,
+		@RequestParam(name = "sortDirection", defaultValue = "ASC") String sortDirection,
+		@RequestParam(name = "by", defaultValue = "name") String by,
+		@RequestParam(value = "id", required = false) UUID id,
+		@RequestParam(name = "productCode", required = false) String productCode,
+		@RequestParam(name = "name", required = false) String name,
+		@RequestParam(name = "manageHubId", required = false) UUID manageHubId,
+		@RequestParam(name = "companyId", required = false) UUID companyId,
+		@RequestParam(name = "companyName", required = false) String companyName,
+		@RequestParam(name = "minPrice", required = false) Integer minPrice,
+		@RequestParam(name = "maxPrice", required = false) Integer maxPrice,
+		@RequestParam(name = "minStock", required = false) Integer minStock,
+		@RequestParam(name = "maxStock", required = false) Integer maxStock,
+		@RequestParam(name = "isDeleted", required = false) Boolean isDeleted
+	){
+
+		Pageable pageable = PageableUtils.customPageable(page, size, sortDirection, by);
+		ProductSearchRequestDto requestDto = new ProductSearchRequestDto(id, productCode, name, manageHubId, companyId, companyName, minPrice, maxPrice, minStock,maxStock, isDeleted);
+
+		Page<ProductSearchResponseServiceDto> responseDto = productService.searchProducts(productPresentationMapper.toProductSearchRequestServiceDto(requestDto), pageable);
+
+		return ResponseEntity.ok(responseDto.map(productPresentationMapper::toProductSearchResponseDto));
+	}
+
+	@Description("마스터 - 상품 상세 조회")
+	@RequiredRoles(UserRoleType.MASTER)
+	@GetMapping("/{id}")
+	public ResponseEntity<ProductDetailsSearchResponseDto> getProductById(
+		@PathVariable UUID id
+	) {
+
+		ProductDetailsSearchRequestServiceDto requestServiceDto = productPresentationMapper.toDetailsSearchServiceDto(id);
+		ProductDetailsSearchResponseServiceDto responseServiceDto = productService.getProductDetails(requestServiceDto);
+		ProductDetailsSearchResponseDto response = productPresentationMapper.toDetailsSearchDto(responseServiceDto);
+		return ResponseEntity.ok(response);
+	}
+
+	@Description("마스터 - 상품 수정")
+	@RequiredRoles(UserRoleType.MASTER)
+	@PutMapping("/{id}")
+	public ResponseEntity<ProductUpdateResponseDto> updateProduct(
+		@PathVariable UUID id,
+		@RequestBody ProductUpdateRequestDto productUpdateRequestDto
+	){
+
+		ProductUpdateRequestServiceDto requestServiceDto = productPresentationMapper.toUpdateServiceDto(id, productUpdateRequestDto);
+		ProductUpdateResponseServiceDto responseServiceDto = productService.updateProduct(requestServiceDto);
+		ProductUpdateResponseDto responseDto = productPresentationMapper.toUpdateResponseDto(responseServiceDto);
+		return ResponseEntity.ok(responseDto);
+	}
+
+	@Description("일반 상품 삭제")
+	@RequiredRoles(UserRoleType.MASTER)
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteProduct(
+		@PathVariable UUID id,
+		HttpServletRequest request
+	){
+		Long userId = (Long) request.getAttribute("userId");
+		ProductDeleteRequestServiceDto requestServiceDto = productPresentationMapper.toDeleteServiceDto(id);
+		productService.deleteProduct(requestServiceDto,userId);
+		return ResponseEntity.noContent().build();
+	}
+}
