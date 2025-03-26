438766653b6de91c920baf03826b9246f4806c7a
ybon1107 <leeyebon7929@gmail.com>
2025-03-27 03:31:46 +0900

    feat: company create api 수정#224
    - create 성공 케이스 test
    - create 실패 케이스 test - 409
    - create 실패 케이스 test - 404

--- company-service/src/test/java/com/oingmaryho/business/companyservice/application/service/CompanyServiceTest.java
+++ company-service/src/test/java/com/oingmaryho/business/companyservice/application/service/CompanyServiceTest.java
@@ -1,6 +1,7 @@
 package com.oingmaryho.business.companyservice.application.service;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.util.List;
@@ -28,10 +29,14 @@ import com.oingmaryho.business.companyservice.application.dto.response.CompanyCr
 import com.oingmaryho.business.companyservice.application.dto.response.CompanyDetailsSearchResponseServiceDto;
 import com.oingmaryho.business.companyservice.application.dto.response.CompanySearchResponseServiceDto;
 import com.oingmaryho.business.companyservice.application.dto.response.CompanyUpdateResponseServiceDto;
+import com.oingmaryho.business.companyservice.application.service.feignClient.HubClient;
 import com.oingmaryho.business.companyservice.domain.Company;
 import com.oingmaryho.business.companyservice.domain.CompanyType;
 import com.oingmaryho.business.companyservice.domain.repository.CompanyRepository;
 import com.oingmaryho.business.companyservice.domain.repository.CustomCompanyRepository;
+import com.oingmaryho.business.companyservice.exception.CompanyException;
+import com.oingmaryho.business.companyservice.exception.ErrorCode;
+import com.oingmaryho.business.companyservice.presentation.dto.response.HubSearchResponseDto;
 
 import jakarta.persistence.EntityNotFoundException;
 import jdk.jfr.Description;
@@ -47,12 +52,15 @@ class CompanyServiceTest {
 	@Mock
 	private CompanyApplicationMapper companyApplicationMapper;
 
+	@Mock
+	private HubClient hubClient;
+
 	@InjectMocks
 	private CompanyService companyService;
 
 	private Company company;
-	private static final UUID FIXED_COMPANY_ID = UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
-	private static final UUID FIXED_MANAGE_HUB_ID = UUID.fromString("000e0000-e89b-12d3-a456-426614174000");
+	private static final UUID FIXED_COMPANY_ID = UUID.fromString("1ebee6c4-5bd8-40c5-a6b4-0b0e8764a269");
+	private static final UUID FIXED_MANAGE_HUB_ID = UUID.fromString("b2341dfa-2e1e-4cb2-a386-6e2d67f02d83");
 	private UUID companyId;
 
 	@BeforeEach
@@ -60,73 +68,144 @@ class CompanyServiceTest {
 		companyId = FIXED_COMPANY_ID;
 		company = Company.builder()
 			.id(FIXED_COMPANY_ID)
-			.name("Test Company")
+			.name("리팩토링업체")
 			.type(CompanyType.SUPPLIER)
-			.managerId(1L)
+			.managerId(7L)
 			.manageHubId(FIXED_MANAGE_HUB_ID)
-			.address("123 Test Street")
+			.address("경기도 고양시 덕양구 355-11")
 			.isDeleted(false)
 			.build();
 	}
 
-	@Description("업체 조회 테스트 - ID로 검색")
+	@Description("업체 생성 테스트 - 성공")
 	@Test
-	void getCompanyById() {
-		// Given
-		CompanyDetailsSearchRequestServiceDto requestDto = new CompanyDetailsSearchRequestServiceDto(companyId);
-
-		// ✅ 올바른 개수의 인자로 객체 생성
-		CompanyDetailsSearchResponseServiceDto expectedResponse = new CompanyDetailsSearchResponseServiceDto(
-			companyId, "Test Company", "Retail",1L, FIXED_MANAGE_HUB_ID, "123 Test Street"
+	void createCompany() {
+		// given
+		CompanyCreateRequestServiceDto request = new CompanyCreateRequestServiceDto(
+			"리팩토링업체", CompanyType.SUPPLIER, 7L, "경기도 고양시 덕양구 355-11"
 		);
 
-		when(companyRepository.findByIdAndIsDeletedFalse(companyId)).thenReturn(Optional.of(company));
-		when(companyApplicationMapper.toResponseDto(company)).thenReturn(expectedResponse);
+		when(hubClient.isManagerOfHub(7L))
+			.thenReturn(Optional.of(new HubSearchResponseDto(FIXED_MANAGE_HUB_ID, "경기 북부 센터","경기도 고양시 덕양구 권율대로 570",37.6403771,126.8737955,7L)));
 
-		// When
-		CompanyDetailsSearchResponseServiceDto response = companyService.getCompanyById(requestDto);
+		when(companyRepository.existsByTypeAndAddressAndIsDeletedFalse(request.type(), request.address()))
+			.thenReturn(false);
+		when(companyApplicationMapper.toCreateEntity(request, FIXED_MANAGE_HUB_ID))
+			.thenReturn(company);
+		when(companyRepository.save(company)).thenReturn(company);
 
-		// Then
-		System.out.println("업체 정보: " + response.name());
+		// when
+		CompanyCreateResponseServiceDto response = companyService.createCompany(request, 7L);
+
+		// then
 		assertThat(response).isNotNull();
-		assertThat(response.name()).isEqualTo("Test Company");
+		assertThat(response.id()).isEqualTo(FIXED_COMPANY_ID);
 	}
 
-	@Description("업체 검색 테스트 - 페이지네이션 포함")
+	@Description("업체 생성 테스트 - 실패(중복된 업체 생성)")
 	@Test
-	void searchCompanies() {
-		Pageable pageable = mock(Pageable.class);
-		CompanySearchRequestServiceDto requestDto = mock(CompanySearchRequestServiceDto.class);
-		Page<Company> companyPage = new PageImpl<>(List.of(company));
-		when(customCompanyRepository.findDynamicQuery(any(), eq(pageable))).thenReturn(companyPage);
-		when(companyApplicationMapper.toCompanySearchResponseServiceDto(any())).thenReturn(mock(
-			CompanySearchResponseServiceDto.class));
-
-		Page<CompanySearchResponseServiceDto> response = companyService.searchCompanies(requestDto, pageable);
-
-		assertThat(response).isNotNull();
-		assertThat(response.getTotalElements()).isEqualTo(1);
-	}
-
-	@Test
-	@Transactional
-	@Description("업체 수정 테스트")
-	void updateCompany_DirtyChecking() {
-		CompanyUpdateRequestServiceDto requestDto = new CompanyUpdateRequestServiceDto(
-			companyId, "Updated Company Name", CompanyType.SUPPLIER, 2L, FIXED_MANAGE_HUB_ID, "456 New Address"
+	void createDuplicateCompany_409() {
+		// given
+		CompanyCreateRequestServiceDto request = new CompanyCreateRequestServiceDto(
+			"리팩토링업체", CompanyType.SUPPLIER, 7L, "경기도 고양시 덕양구 355-11"
 		);
 
-		when(companyRepository.findByIdAndIsDeletedFalse(companyId)).thenReturn(Optional.of(company));
-		when(companyApplicationMapper.toUpdateResponseDto(any(UUID.class)))
-			.thenReturn(new CompanyUpdateResponseServiceDto(FIXED_COMPANY_ID));
+		when(hubClient.isManagerOfHub(7L))
+			.thenReturn(Optional.of(new HubSearchResponseDto(FIXED_MANAGE_HUB_ID, "경기 북부 센터", "경기도 고양시 덕양구 권율대로 570", 37.6403771, 126.8737955, 7L)));
 
-		CompanyUpdateResponseServiceDto response = companyService.updateCompany(2L, UserRoleType.HUB_MANAGER,requestDto);
+		when(companyRepository.existsByTypeAndAddressAndIsDeletedFalse(request.type(), request.address()))
+			.thenReturn(true);
 
-		assertThat(company.getName()).isEqualTo("Updated Company Name");
-		assertThat(company.getAddress()).isEqualTo("456 New Address");
-		assertThat(response).isNotNull();
-		assertThat(response.id()).isEqualTo(companyId);
+		// when & then
+		CompanyException exception = assertThrows(CompanyException.class, () -> {
+			companyService.createCompany(request, 7L);
+		});
+
+		assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.ALREADY_REGISTERED_COMPANY);
+
+		verify(companyRepository, never()).save(any());
 	}
+
+
+	@Description("업체 생성 테스트 - 마스터 존재하지 않는 허브(매니저)")
+	@Test
+	void createCompany_404() {
+		// given
+		CompanyCreateRequestServiceDto request = new CompanyCreateRequestServiceDto(
+			"리팩토링업체", CompanyType.SUPPLIER, 6L, "경기도 고양시 덕양구 355-11"
+		);
+
+		when(hubClient.isManagerOfHub(6L))
+			.thenReturn(Optional.empty());
+
+		// when & then
+		CompanyException exception = assertThrows(CompanyException.class, () -> {
+			companyService.createCompany(request, 6L);
+		});
+
+		assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.HUB_NOT_FOUND);
+
+		verify(companyRepository, never()).save(any());
+	}
+
+	// @Description("업체 조회 테스트 - ID로 검색")
+	// @Test
+	// void getCompanyById() {
+	// 	// Given
+	// 	CompanyDetailsSearchRequestServiceDto requestDto = new CompanyDetailsSearchRequestServiceDto(companyId);
+	//
+	// 	// ✅ 올바른 개수의 인자로 객체 생성
+	// 	CompanyDetailsSearchResponseServiceDto expectedResponse = new CompanyDetailsSearchResponseServiceDto(
+	// 		companyId, "Test Company", "Retail",1L, FIXED_MANAGE_HUB_ID, "123 Test Street"
+	// 	);
+	//
+	// 	when(companyRepository.findByIdAndIsDeletedFalse(companyId)).thenReturn(Optional.of(company));
+	// 	when(companyApplicationMapper.toResponseDto(company)).thenReturn(expectedResponse);
+	//
+	// 	// When
+	// 	CompanyDetailsSearchResponseServiceDto response = companyService.getCompanyById(requestDto);
+	//
+	// 	// Then
+	// 	System.out.println("업체 정보: " + response.name());
+	// 	assertThat(response).isNotNull();
+	// 	assertThat(response.name()).isEqualTo("Test Company");
+	// }
+	//
+	// @Description("업체 검색 테스트 - 페이지네이션 포함")
+	// @Test
+	// void searchCompanies() {
+	// 	Pageable pageable = mock(Pageable.class);
+	// 	CompanySearchRequestServiceDto requestDto = mock(CompanySearchRequestServiceDto.class);
+	// 	Page<Company> companyPage = new PageImpl<>(List.of(company));
+	// 	when(customCompanyRepository.findDynamicQuery(any(), eq(pageable))).thenReturn(companyPage);
+	// 	when(companyApplicationMapper.toCompanySearchResponseServiceDto(any())).thenReturn(mock(
+	// 		CompanySearchResponseServiceDto.class));
+	//
+	// 	Page<CompanySearchResponseServiceDto> response = companyService.searchCompanies(requestDto, pageable);
+	//
+	// 	assertThat(response).isNotNull();
+	// 	assertThat(response.getTotalElements()).isEqualTo(1);
+	// }
+	//
+	// @Test
+	// @Transactional
+	// @Description("업체 수정 테스트")
+	// void updateCompany_DirtyChecking() {
+	// 	CompanyUpdateRequestServiceDto requestDto = new CompanyUpdateRequestServiceDto(
+	// 		companyId, "Updated Company Name", CompanyType.SUPPLIER, 2L, FIXED_MANAGE_HUB_ID, "456 New Address"
+	// 	);
+	//
+	// 	when(companyRepository.findByIdAndIsDeletedFalse(companyId)).thenReturn(Optional.of(company));
+	// 	when(companyApplicationMapper.toUpdateResponseDto(any(UUID.class)))
+	// 		.thenReturn(new CompanyUpdateResponseServiceDto(FIXED_COMPANY_ID));
+	//
+	// 	CompanyUpdateResponseServiceDto response = companyService.updateCompany(2L, UserRoleType.HUB_MANAGER,requestDto);
+	//
+	// 	assertThat(company.getName()).isEqualTo("Updated Company Name");
+	// 	assertThat(company.getAddress()).isEqualTo("456 New Address");
+	// 	assertThat(response).isNotNull();
+	// 	assertThat(response.id()).isEqualTo(companyId);
+	// }
 
 
 }
